7977626dc76f863f29ef43ef0cf7337ed0536328
Olga GSF <tmpl-toest@yandex.ru>
2022-05-23 17:11:40 +0300

    Modified after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,21 @@
-const editButton = document.querySelector('.button_type_edit')
+const editButton = document.querySelector('.profile__button-edit')
 const popup = document.querySelector('.popup')
-const closePopupButton = document.querySelector('.button_type_close')
+const closePopupButton = document.querySelector('.popup__button-close')
 const titleElement = document.querySelector('.profile__title')
 const subtitleElement = document.querySelector('.profile__subtitle')
-const nameFieldElement = document.querySelector('.popup__input_name')
-const descriptionFieldElement = document.querySelector('.popup__input_description')
+const nameFieldElement = document.querySelector('.popup__input_name_firstname')
+const descriptionFieldElement = document.querySelector('.popup__input_name_description')
 const formElement = document.querySelector('.popup__form')
 /*const likedElement = document.querySelector('.button_type_like')*/
 
 function openPopup(popupElement) {
-  popupElement.classList.add('popup_isOpen')
+  popupElement.classList.add('popup__is-open')
+  nameFieldElement.value = titleElement.textContent;
+  descriptionFieldElement.value = subtitleElement.textContent;
 }
 
 function closePopup(popupElement) {
-  popupElement.classList.remove('popup_isOpen')
+  popupElement.classList.remove('popup__is-open')
 }
 
 /*function liked(likedElement) {
@@ -22,14 +24,10 @@ function closePopup(popupElement) {
 
 editButton.addEventListener('click', function () {
   openPopup(popup)
-  nameFieldElement.value = titleElement.textContent;
-  descriptionFieldElement.value = subtitleElement.textContent;
 })
 
 closePopupButton.addEventListener('click', function () {
   closePopup(popup)
-  titleElement.textContent = nameFieldElement.value;
-  subtitleElement.textContent = descriptionFieldElement.value;
 })
 
 formElement.addEventListener('submit', function (event) {
